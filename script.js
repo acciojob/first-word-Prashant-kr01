897a18143d1de041374996c1d9e02bdd0a95f401
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-02 22:45:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	let news ="";
+	
+	for(let el of s ){
+	if(el==" "){
+		break;
+	}else{
+		news += el;
+	}
+		
+	}
+	return news;
 }
 
-// Do not change the code below
+// Do not change the code belo
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
